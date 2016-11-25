50433fab7bee7fe148733927f766ba1bf0a135ab
Isis de Oliveira <Isis de Oliveira>
2016-11-24 22:02:59 -0200

    Primeiro commit

---
+++ Notas.c
@@ -0,0 +1,39 @@
+#include <stdio.h> //printf
+#include <stdlib.h> //limpa a tela
+
+/* 
+1 - Cadastro de alunos
+2 - Cadastro de notas
+3 - Cálculo das médias
+4 - Relatório de desempenho
+*/
+
+
+struct Aluno{  
+
+	// Declaração de variáveis
+	
+	int ra;
+	float nota[3];
+	float media;
+	
+	
+};
+
+main () {
+	struct Aluno;
+	
+	char nome[30];
+	
+	printf("***** CADASTRO ALUNO *****\n\n\n\n");
+	printf("Digite o nome do aluno:\n");
+	scanf (nome);
+	printf("Digite o RA:");
+	scanf(ra);
+	printf("Digite a nota 1:");
+	
+
+
+	
+}
+
